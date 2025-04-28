7b41d6087e14359135ff3eb1990927b4a576e57b
ragini412 <raginigandharv05@gmail.com>
2025-04-28 14:05:00 -0400

    second commit

--- script.js
+++ script.js
@@ -1,43 +1,86 @@
-// carousel js
+// // carousel js
+
+// const carousel = document.querySelector(".carousel");
+// const arrowbtns = document.querySelectorAll(".carousel i");
+// const firstCardWidth = carousel.querySelector(".carousel-img").offsetWidth;
+
+// let isdragging = false, startX , startScrollLeft;
+
+// arrowbtns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         // console.log(btn.id);
+//         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;    
+//     })
+// });
+
+// const dragStart = (e) => {
+//     isdragging = true;
+//     carousel.classList.add("dragging");
+//     startX= e.pageX;
+//     startScrollLeft=carousel.scrollLeft;
+// }
+
+// const dragging = (e) => {
+//     if(!isdragging) return; // If isdragging is false return from here!
+//     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+//     // carousel.scrollLeft = e.pageX;
+// }
+
+// const dragStop = (e) => {
+//     isdragging = true;
+//     carousel.classList.remove("dragging");
+// }
+
+// // carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("mouseover", dragStart);
+// carousel.addEventListener("mousemove", dragging); 
+// document.addEventListener("mouseup",dragStop);
+
+//For touch
+// carousel.addEventListener("ontouchstart", dragStart);
+// carousel.addEventListener("ontouchmove", dragging); 
+// document.addEventListener("ontouchend",dragStop);
 
 const carousel = document.querySelector(".carousel");
 const arrowbtns = document.querySelectorAll(".carousel i");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const firstCardWidth = carousel.querySelector(".carousel-img").offsetWidth;
 
-let isdragging = false, startX , startScrollLeft;
+let isDragging = false, startX, startScrollLeft;
 
+// Arrow button scrolling
 arrowbtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        // console.log(btn.id);
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;    
-    })
+    });
 });
 
+// Drag start
 const dragStart = (e) => {
-    isdragging = true;
+    isDragging = true;
     carousel.classList.add("dragging");
-    startX= e.pageX;
-    startScrollLeft=carousel.scrollLeft;
-}
+    startX = e.pageX || e.touches[0].pageX; // Support mouse and touch
+    startScrollLeft = carousel.scrollLeft;
+};
 
+// Dragging
 const dragging = (e) => {
-    if(!isdragging) return; // If isdragging is false return from here!
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-    // carousel.scrollLeft = e.pageX;
-}
+    if (!isDragging) return;
+    const x = e.pageX || e.touches[0].pageX;
+    carousel.scrollLeft = startScrollLeft - (x - startX);
+};
 
-const dragStop = (e) => {
-    isdragging = true;
+// Drag stop
+const dragStop = () => {
+    isDragging = false;
     carousel.classList.remove("dragging");
-}
+};
 
-// carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mouseover", dragStart);
-carousel.addEventListener("mousemove", dragging); 
-document.addEventListener("mouseup",dragStop);
+// Mouse events
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
 
-//For touch
-carousel.addEventListener("ontouchstart", dragStart);
-carousel.addEventListener("ontouchmove", dragging); 
-document.addEventListener("ontouchend",dragStop);
-
+// Touch events
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("touchend", dragStop);
